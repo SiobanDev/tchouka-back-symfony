fd3b43d51cd4831b8bdf78cde761ead47a97719a
sunsay_dev <sunsay.dev@gmail.com>
2020-09-21 09:22:16 +0200

    fix bug imports

--- src/Entity/Composition.php
+++ src/Entity/Composition.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CompositionRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CompositionRepository::class)
